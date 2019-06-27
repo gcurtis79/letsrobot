de32e424cfe169a881799b86194378122f22b248
Greg Curtis <gcurtis79@gmail.com>
2019-06-26 19:57:35 -0500

    yes

--- tts/espeak.py
+++ tts/espeak.py
@@ -5,27 +5,29 @@ import logging
 
 log = logging.getLogger('LR.tts.espeak')
 
+tempDir = None
 male = None
 voice_number = None
 hw_num = None
-custom_voice = None
-tempDir = None
-
-max_queue = False
-playing = False
-_message_queue = []
 
 def setup(robot_config):
+    global tempDir
     global male
     global voice_number
     global hw_num
-    global custom_voice
 
     male = robot_config.getboolean('espeak', 'male')
     voice_number = robot_config.getint('espeak', 'voice_number')
-    hw_num = robot_config.getint('tts', 'hw_num')
-    custom_voice = robot_config.get('espeak', 'custom_voice')
+    
+    if robot_config.has_option('tts', 'speaker_num'):
+        hw_num = robot_config.get('tts', 'speaker_num')
+    else:
+        hw_num = robot_config.get('tts', 'hw_num')
 
+
+    #set the location to write the temp file to
+    tempDir = tempfile.gettempdir()
+    log.info("TTS temporary directory : %s", tempDir)
 
 def say(*args):
     message = args[0]
@@ -35,7 +37,7 @@ def say(*args):
     f.close()
 
     if male:
-        os.system('cat ' + tempFilePath + ' | espeak -v en-us+m%d -s 170 --stdout | aplay -D plughw:%d,0' %(voice_number, hw_num) )
+        os.system('cat ' + tempFilePath + ' | espeak -v en-us+m{} -s 170 --stdout | aplay -D plughw:{}'.format(voice_number, hw_num) )
     else:
-        os.system('cat ' + tempFilePath + ' | espeak -v en-us+f%d -s 170 --stdout | aplay -D plughw:%d,0' % (voice_number, hw_num) )
+        os.system('cat ' + tempFilePath + ' | espeak -v en-us+f{} -s 170 --stdout | aplay -D plughw:{}'.format(voice_number, hw_num) )
     os.remove(tempFilePath)    
