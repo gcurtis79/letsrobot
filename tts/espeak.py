c9af57f58ed97b45ab67de63e5e2709ad493c3fc
Greg Curtis <gcurtis79@gmail.com>
2019-06-26 19:51:54 -0500

    ...

--- tts/espeak.py
+++ tts/espeak.py
@@ -28,26 +28,6 @@ def setup(robot_config):
 
 def say(*args):
     message = args[0]
-<<<<<<< HEAD
-    global playing
-    if not max_queue and len(_message_queue) <= max_queue:  # Queue length control
-        _message_queue.append(message)
-    if playing:  # If playing, add to queue and exit
-        return
-    playing = True
-    while len(_message_queue) > 0:  # Keep playing until queue is exhausted
-        saythis = _message_queue.pop(0).lstrip()
-        #cmdLine = ('wget --header "Referer: http://translate.google.com/" --header "User-Agent: stagefright/1.2 (Linux;Android 5.0)" -qO- "https://translate.google.com/translate_tts?ie=UTF-8&q={msg}&tl={lang}&client=tw-ob" | ffplay -loglevel quiet -nodisp -autoexit -').format(msg=message, lang=google_language)
-        if male:
-            voice = ('en-us+m%d' % (voice_number))
-        if not male:
-            voice = ('en-us+f%d -s170' % (voice_number))
-        if custom_voice:
-            voice = ('%s' % (custom_voice))
-        cmdLine = ('espeak -v%s -s170 --stdout "%s" 2>/dev/null | aplay -q -D plughw:%d,0' % (voice, str(saythis), hw_num))
-        os.devnull = os.system(cmdLine)
-    playing = False
-=======
     tempFilePath = os.path.join(tempDir, "text_" + str(uuid.uuid4()))
     f = open(tempFilePath, "w")
     f.write(message)
@@ -58,4 +38,3 @@ def say(*args):
     else:
         os.system('cat ' + tempFilePath + ' | espeak -v en-us+f%d -s 170 --stdout | aplay -D plughw:%d,0' % (voice_number, hw_num) )
     os.remove(tempFilePath)    
->>>>>>> 0111c17a2f88e8b94faaf3629302034dd5b387d7
